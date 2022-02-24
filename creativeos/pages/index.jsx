f7b1c1912d3c10c058e3cdc9703db84d83b1ab15
Retr0A <92212390+Retr0A@users.noreply.github.com>
2022-02-24 13:26:27 +0200

    Added Next.js

---
+++ creativeos/pages/index.jsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="index">
+      <Head>
+        <title>CreativeOS</title>
+        <meta name="description" content="The Web OS" />
+      </Head>
+    </div>
+  )
+}
