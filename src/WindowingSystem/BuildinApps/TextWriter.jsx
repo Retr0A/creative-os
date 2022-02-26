2fff9e65d8f3a3d1fa2c9432c2441437b3e49a75
Retr0A <92212390+Retr0A@users.noreply.github.com>
2022-02-26 17:17:32 +0200

    Little Netlify Fix

--- src/WindowingSystem/BuildinApps/TextWriter.jsx
+++ src/WindowingSystem/BuildinApps/TextWriter.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Draggable, {DraggableCore} from 'react-draggable'
+import Draggable from 'react-draggable'
 import '../Window.scss'
 
 export default class TextWriter extends React.Component {
